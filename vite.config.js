a18b24e267d6cd771102f2fc9f5f4901b182daac
mev <mev0145@gmail.com>
2025-10-26 13:12:10 +0900

    add: 빌드 react-snap

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,21 @@
 /// <reference types="vitest/config" />
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vite.dev/config/
-import path from 'node:path';
+import path from 'node:path'; // path 모듈 전체 임포트
 import { fileURLToPath } from 'node:url';
 import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
-const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename); // path.dirname을 사용하여 __dirname 정의
+import react from '@vitejs/plugin-react';
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(dirname, './src'),
+      '@': path.resolve(__dirname, './src'),
     },
   },
+
   server: {
     host: '0.0.0.0',
     allowedHosts: ['mev.o-r.kr']
@@ -27,7 +27,7 @@ export default defineConfig({
       // The plugin will run tests for the stories defined in your Storybook config
       // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
       storybookTest({
-        configDir: path.join(dirname, '.storybook')
+        configDir: path.join(__dirname, '.storybook')
       })],
       test: {
         name: 'storybook',
